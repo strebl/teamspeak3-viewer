f944f7f0d3fb6c7a0b18ce828403778dee27d36f
Manuel Strebel <manuel_st@msn.com>
2018-02-08 23:25:24 +0000

    Apply fixes from StyleCI

--- app/Providers/TeamspeakServiceProvider.php
+++ app/Providers/TeamspeakServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Teamspeak\Teamspeak;
+use Illuminate\Support\ServiceProvider;
 use TeamSpeak3;
 
 class TeamspeakServiceProvider extends ServiceProvider
